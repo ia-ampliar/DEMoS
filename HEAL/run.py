c2c89f35f1192741e63dd7a28226fb44cb79cc72
Rodrigo <rsb6@cin.ufpe.br>
2024-09-09 20:45:56 -0300

    modify code and test the tiling

--- HEAL/run.py
+++ HEAL/run.py
@@ -25,7 +25,41 @@ def load_variable(filename):
 
 def run(**kwargs):
     """
-    Main entry point of the HEAL package.
+    This is the main entry point of the HEAL package.
+    Default parameters:
+        label_file = "label_file.csv";
+        testing_label_file = None;
+        models = ['ResNet50'];
+        training_mode = "single_round";
+        procedure = ["Tiling", "Data_Split", "Training",  "Testing", "Survival_analysis"].
+
+    **parameter description**
+        1) label_file: (string) the file path of the input csv format file. The csv file
+        contains two columns,
+            col_1: "Image_path" (the image file paths of each patient)
+            col_2: "Label" (the label of the corresponding patient, each sample could
+                contains several labels.)
+        2) testing_label_file: (string) the file path of the input csv format file for
+         model training.
+            This parameter is None as default, the testing dataset will be split from
+            the "label file".
+            If the user input test file, this file will be used for independent test.
+        3) models: (list) specify the deep learning models used for model training.
+            The available options are: VGG11, VGG19, ResNet36, ResNet50, Inception-V3,
+            SENet, ResNeXt ...;
+        4) training_mode: (string) specify the training mode, single round training or
+        10-fold cross-validation;
+        5) procedure: (list) specify the detailed processing steps for the model training.
+        The entire processing steps is consisted by 5 steps:
+                a) tiling: segment the whole slide image into small patches, the default
+                image size is 1000*1000;
+                b) data_split: split the dataset into training dataset, validation dataset,
+                and testing dataset;
+                c) training: to train the model based on the input images and the specified
+                models;
+                d) testing: to test the model performance based on the optimised model;
+                e) survival_analysis: (Optional) to conduct the survival analysis based on
+                 the output of the independent test.
     """
     print(kwargs.items())
 
@@ -86,6 +120,6 @@ if __name__ == "__main__":
         label_file="/home/rsb6/Desktop/Trabalho/DEMoS/HEAL/datas/label_file.csv",
         testing_label_file=None,
         models=['ResNet50'],
-        procedure=["Tiling"],
+        procedure=["Pre_processing"],
         tile_info=[256, 0]  # Exemplos de valores
     )
