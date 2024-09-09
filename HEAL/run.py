77338b3a434e893e54afe45d90090005198a774c
Rodrigo <rsb6@cin.ufpe.br>
2024-09-09 20:40:27 -0300

    modify code and test tiling

--- HEAL/run.py
+++ HEAL/run.py
@@ -1,18 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""
-from HEAL.HEAL_APP import tiling
-from HEAL.HEAL_APP import train
-from HEAL.HEAL_APP import cross_validation
-from HEAL.HEAL_APP import independent_test
-from HEAL.HEAL_APP import survival_analysis
-
-"""
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import pickle
 from Tiling import tiling
 from Pre_processing import pre_processing
@@ -25,98 +12,42 @@ from Grad_Cam import grad_cam
 
 
 def save_variable(var, filename):
-    pickle_f = open(filename, 'wb')
-    pickle.dump(var, pickle_f)
-    pickle_f.close()
+    with open(filename, 'wb') as pickle_f:
+        pickle.dump(var, pickle_f)
     return filename
 
 
 def load_variable(filename):
-    pickle_f = open(filename, 'rb')
-    var = pickle.load(pickle_f)
-    pickle_f.close()
+    with open(filename, 'rb') as pickle_f:
+        var = pickle.load(pickle_f)
     return var
 
 
 def run(**kwargs):
     """
-    This is the main entry point of the HEAL package.
-    Default parameters:
-        label_file = "label_file.csv";
-        testing_label_file = None;
-        models = ['ResNet50'];
-        training_mode = "single_round";
-        procedure = ["Tiling", "Data_Split", "Training",  "Testing", "Survival_analysis"].
-
-    **parameter description**
-        1) label_file: (string) the file path of the input csv format file. The csv file
-        contains two columns,
-            col_1: "Image_path" (the image file paths of each patient)
-            col_2: "Label" (the label of the corresponding patient, each sample could
-                contains several labels.)
-        2) testing_label_file: (string) the file path of the input csv format file for
-         model training.
-            This parameter is None as default, the testing dataset will be split from
-            the "label file".
-            If the user input test file, this file will be used for independent test.
-        3) models: (list) specify the deep learning models used for model training.
-            The available options are: VGG11, VGG19, ResNet36, ResNet50, Inception-V3,
-            SENet, ResNeXt ...;
-        4) training_mode: (string) specify the training mode, single round training or
-        10-fold cross-validation;
-        5) procedure: (list) specify the detailed processing steps for the model training.
-        The entire processing steps is consisted by 5 steps:
-                a) tiling: segment the whole slide image into small patches, the default
-                image size is 1000*1000;
-                b) data_split: split the dataset into training dataset, validation dataset,
-                and testing dataset;
-                c) training: to train the model based on the input images and the specified
-                models;
-                d) testing: to test the model performance based on the optimised model;
-                e) survival_analysis: (Optional) to conduct the survival analysis based on
-                 the output of the independent test.
-
+    Main entry point of the HEAL package.
     """
-    # print(kwargs.items())
+    print(kwargs.items())
+
     # Parse the input parameters
-    _label_file = None
-    _testing_label_file = None
-    _models = None
-    _training_mode = None
-    _procedure = None
-    _tile_info = None
-    _filter_model = None
-    _extra_test_file = None
-    _extra_testing_pre_processing_enable = False
-    for _key, _val in kwargs.items():
-        # print(_key, _val)
-        if _key == "label_file":
-            _label_file = _val
-        elif _key == "tile_info":
-            _tile_info = _val
-        elif _key == "filter_model":
-            _filter_model = _val
-        elif _key == "testing_label_file":
-            _testing_label_file = _val
-        elif _key == "extra_testing_label_file":
-            _extra_test_file = _val
-        elif _key == "extra_testing_pre_processing_enable":
-            _extra_testing_pre_processing_enable = _val
-        elif _key == "models":
-            _models = _val
-        elif _key == "training_mode":
-            _training_mode = _val
-        elif _key == "procedure":
-            _procedure = _val
-    _label_file_tiled = None
-    if _label_file is not None:
-        _label_file_tiled = "HEAL_Workspace/outputs/label_file_tiled.csv"
+    _label_file = kwargs.get("label_file", None)
+    _testing_label_file = kwargs.get("testing_label_file", None)
+    _models = kwargs.get("models", None)
+    _training_mode = kwargs.get("training_mode", "Single_round")
+    _procedure = kwargs.get("procedure", None)
+    _tile_info = kwargs.get("tile_info", [256, 0])  # Default values for tile size and level
+    _filter_model = kwargs.get("filter_model", None)
+    _extra_test_file = kwargs.get("extra_testing_label_file", None)
+    _extra_testing_pre_processing_enable = kwargs.get("extra_testing_pre_processing_enable", False)
 
-    _test_label_file_tiled = None
-    if _testing_label_file is not None:
-        _test_label_file_tiled = "HEAL_Workspace/outputs/test_label_file_tiled.csv"
+    _label_file_tiled = "HEAL_Workspace/outputs/label_file_tiled.csv" if _label_file else None
+    _test_label_file_tiled = "HEAL_Workspace/outputs/test_label_file_tiled.csv" if _testing_label_file else None
 
-    # Call the corresponding functions based on the specified procedures.
+    # Create necessary directories if they do not exist
+    os.makedirs('HEAL_Workspace/tiling/train', exist_ok=True)
+    os.makedirs('HEAL_Workspace/outputs', exist_ok=True)
+
+    # Call the corresponding functions based on the specified procedures
     for _proc in _procedure:
         if _proc == "Tiling":
             print("[INFO] Start tiling ...")
@@ -149,3 +80,12 @@ def run(**kwargs):
             grad_cam.grad_cam()
 
     print(_label_file, _testing_label_file, _models, _training_mode, _procedure)
+
+if __name__ == "__main__":
+    run(
+        label_file="/home/rsb6/Desktop/Trabalho/DEMoS/HEAL/datas/label_file.csv",
+        testing_label_file=None,
+        models=['ResNet50'],
+        procedure=["Tiling"],
+        tile_info=[256, 0]  # Exemplos de valores
+    )
