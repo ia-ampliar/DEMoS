68c03de728de1a8091d36a4c809cf04ca28d8acf
ampliar <ia.ampliar@gmail.com>
2025-05-05 21:28:53 -0300

    modify pre-processessing module

--- HEAL/Pre_processing/pre_processing.py
+++ HEAL/Pre_processing/pre_processing.py
@@ -6,22 +6,17 @@ import cv2 as cv
 import multiprocessing
 import numpy as np
 
-# Verifica se o OpenCV foi compilado com suporte a CUDA
-if cv.cuda.getCudaEnabledDeviceCount() == 0:
-    raise RuntimeError("OpenCV não foi compilado com suporte a CUDA. Instale o OpenCV com suporte a CUDA.")
-
 def create_new_folder(_patient_path, ori_str, replace_str):
     _new_patient_path = re.sub(ori_str, replace_str, _patient_path)
     Path(_new_patient_path).mkdir(parents=True, exist_ok=True)
     return _new_patient_path
 
 def variance_of_laplacian(image):
-    # Converte a imagem para escala de cinza na GPU
-    gpu_gray = cv.cuda.cvtColor(image, cv.COLOR_BGR2GRAY)
-    # Aplica o Laplacian na GPU
-    gpu_laplacian = cv.cuda.Laplacian(gpu_gray, cv.CV_64F)
-    # Baixa o resultado para a CPU e calcula a variância
-    laplacian = gpu_laplacian.download()
+    # Converte a imagem para escala de cinza na CPU
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # Aplica o Laplacian na CPU
+    laplacian = cv.Laplacian(gray, cv.CV_64F)
+    # Calcula a variância
     return laplacian.var()
 
 def find_blur(imagePath):
@@ -33,17 +28,13 @@ def find_blur(imagePath):
         print(f"Failed to read image at: {imagePath}")
         return None, 0
 
-    # Upload da imagem para a GPU
-    gpu_image = cv.cuda_GpuMat()
-    gpu_image.upload(image)
-
-    # Calcula o desfoque na GPU
-    fm = variance_of_laplacian(gpu_image)
-    return gpu_image, fm
+    # Calcula o desfoque na CPU
+    fm = variance_of_laplacian(image)
+    return image, fm
 
 def blur_color_processing(_root, _img_path, _img, _img_path_template, _blur_threshold=1000):
-    gpu_image, fm = find_blur(_img_path)
-    if gpu_image is None:
+    image, fm = find_blur(_img_path)
+    if image is None:
         return fm  # Se a imagem não pôde ser lida, retorna diretamente
 
     print(f"Processing: {_img_path}, Blur score: {fm}")
@@ -94,7 +85,7 @@ def blur_color_processing(_root, _img_path, _img, _img_path_template, _blur_thre
     return fm
 
 # Caminho relativo ao diretório do projeto
-relative_path = "/mnt/efs-tcga/DEMoS/HEAL/Pre_processing/img_template.png"
+relative_path = "DEMoS/HEAL/Pre_processing/img_template.png"
 project_base = os.getcwd()
 # Construir o caminho absoluto
 absolute_path = os.path.join(project_base, relative_path)
@@ -105,7 +96,7 @@ def pre_processing(extra_prefix="", _img_path_template=absolute_path):
     print(f"The CPU number of this machine is {cpu_num}")
     pool = multiprocessing.Pool(cpu_num)
 
-    _image_path = "/mnt/efs-tcga/HEAL_Workspace/tiling" + str(extra_prefix)
+    _image_path = "/mnt/efs-tcga/tiling/" + str(extra_prefix)
     for _root, _dir, _imgs in os.walk(_image_path):
         _imgs = [f for f in _imgs if not f[0] == '.']
         _dir[:] = [d for d in _dir if not d[0] == '.']
